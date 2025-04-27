9952b582c3c4af58a08d69e908734cded1d9b4ad
Bulbozauras6 <badiup12@gmail.com>
2025-04-28 00:44:38 +0300

    Profile.tsx

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import './Styles.css'; // Importuojame CSS failą
 
 const Profile: React.FC = () => {
     return (
-        <div>
-            <h1>Profile Page</h1>
-            <p>Welcome to your profile!</p>
+        <div className="page_Container">
+            <div className="title">Profilis</div>
+
+            <div className="container">
+                <div className="form-title">Player Information</div>
+                <div className="form-box">
+                    <div className="input-container">
+                        <strong>Nickname:</strong> <span>&nbsp;Testas</span>
+                    </div>
+                    <div className="input-container">
+                        <strong>Games Played: </strong> <span>&nbsp;120</span>
+                    </div>
+                    <div className="input-container">
+                        <strong>Wins: </strong> <span>&nbsp;85</span>
+                    </div>
+                    <div className="input-container">
+                        <strong>Losses: </strong> <span>&nbsp;35</span>
+                    </div>
+                    <div className="input-container">
+                        <strong>Current Balance: </strong> <span>&nbsp;$500</span>
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
